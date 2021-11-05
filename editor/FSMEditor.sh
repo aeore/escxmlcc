4a641dbcb7b2d66c88c13a6e93412532713c02e9
Yuriy Gurin <ygurin@outlook.com>
2021-11-06 01:27:41 +0200

    Adding editor dependency binaries

--- editor/FSMEditor.sh
+++ editor/FSMEditor.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-java -jar --add-modules java.xml.bind fsm-editor.jar
+# for old jre
+# java -jar fsm-editor.jar
+
+# for new jre
+java -cp "jaxb-api-2.3.0.jar:jaxb-core-2.3.0.jar:jaxb-impl-2.3.0.jar:fsm-editor.jar:activation-1.0.2.jar"  com.mxgraph.examples.swing.SCXMLGraphEditor
